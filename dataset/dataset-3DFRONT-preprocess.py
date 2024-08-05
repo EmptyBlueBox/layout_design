04275e1ab03c2e8889325850be003cc4795e10c8
Yutong Liang <lyt0112@sc.stanford.edu>
2024-08-05 16:42:27 -0700

    feat: 3D-FRONT dataset parse done

--- dataset/dataset-3DFRONT-preprocess.py
+++ dataset/dataset-3DFRONT-preprocess.py
@@ -1,16 +1,16 @@
 import json
-import config
 import categories
 import os
 import numpy as np
 import rerun as rr
 from tqdm import tqdm
 import re
+import config
+from utils.mesh_utils import compute_vertex_normals
 
-MIN_OBJ_NUM = 4 # 一个房间中最少的物体数量
+MIN_OBJ_NUM = 5 # 一个房间中最少的物体数量
 save_path = f'./3DFRONT-preprocessed-MIN_OBJ_NUM_{MIN_OBJ_NUM}/'
-# choose_room_types = 'bedroom'
-# choose_room_types = 'livingroom'
+SAVE_HOUSE = False
 
 
 def get_model_id_2_info(info_path):
@@ -30,20 +30,11 @@ def main():
     catagory_name_2_super = {entey['category']: entey['super-category'] for entey in categories._CATEGORIES_3D}
     catagory_num = len(catagory_name_2_super)
 
-    # if choose_room_types == 'bedroom':
-    #     chosen_room_types = ['Bedroom', 'MasterBedroom', 'SecondBedroom']
-    # if choose_room_types == 'living':
-    #     chosen_room_types = ['LivingDiningRoom', 'LivingRoom']
-    # layout_room_dict = {k: [] for k in chosen_room_types}
-    # test_room_dict = {k: None for k in chosen_room_types}  # 用于保存测试房间信息
     all_room_dict = {}  # 用于保存所有房间信息
 
     layouts = os.listdir(config.DATASET_3DFRONT_LAYOUT_PATH)
     layouts.sort()
     for layout in tqdm(layouts):
-        # print(len(all_room_dict))
-        # if len(all_room_dict) == 10: # 测试用, 保证只有20种房间类型
-        #     break
         layout_path = os.path.join(config.DATASET_3DFRONT_LAYOUT_PATH, layout)
         with open(layout_path, 'r', encoding='utf-8') as f:
             layout_info = json.load(f)
@@ -60,26 +51,54 @@ def main():
 
         # mesh refers to wall/floor/etc
         mesh_uid = []
-        mesh_xyz = []
+        mesh_vertices = []
         mesh_faces = []
+        # print(f'mesh keys: {layout_info["mesh"][0].keys()}')
+        # print(f'mesh aid: {layout_info["mesh"][0]["aid"]}')
+        # print(f'mesh jid: {layout_info["mesh"][0]["jid"]}')
+        # print(f'mesh type: {layout_info["mesh"][0]["type"]}')
+        # print(f'mesh constructid: {layout_info["mesh"][0]["constructid"]}')
+        if SAVE_HOUSE:
+            rr.init(f'3DFRONT-layout-{layout}')
+            rr.save(os.path.join(save_path, f'layout-{layout}.rrd'))
+            rr.log('', rr.ViewCoordinates.RIGHT_HAND_Y_UP, static=True)
         for mesh in layout_info['mesh']:
             mesh_uid.append(mesh['uid'])
-            mesh_xyz.append(np.reshape(mesh['xyz'], [-1, 3]))
+            mesh_vertices.append(np.reshape(mesh['xyz'], [-1, 3]))
             mesh_faces.append(np.reshape(mesh['faces'], [-1, 3]))
-
+            
+            # print(f'mesh uid: {mesh["uid"]}')
+            # print(f'    mesh aid: {mesh["aid"]}')
+            # print(f'    mesh jid: {mesh["jid"]}')
+            # print(f'    mesh type: {mesh["type"]}')
+            # print(f'    mesh constructid: {mesh["constructid"]}')
+            if SAVE_HOUSE:
+                if mesh["type"] == 'Ceiling':
+                    rr.log(f'Ceilings/{mesh_uid[-1]}', rr.Mesh3D(
+                    vertex_positions=mesh_vertices[-1],
+                    triangle_indices=mesh_faces[-1],
+                    vertex_normals=compute_vertex_normals(mesh_vertices[-1], mesh_faces[-1])
+                    ))
+            
+        # print(f'mesh num: {len(mesh_uid)}')
         scene = layout_info['scene']
+        # print(f'scene keys: {scene.keys()}')
         rooms = scene['room']
+        # print(f'room num: {len(rooms)}')
+        # print(f'room keys: {rooms[0].keys()}')
         for room in rooms:
             # 如果房间类型不在指定的房间类型列表中，则跳过
-            # if room['type'] not in chosen_room_types:
-            #     continue
-
+            # print(f'room id: {room["instanceid"]}')
+            # print(f'    room type: {room["type"]}')
+            # print(f'    room size: {room["size"]}')
+            # print(f'    room pos: {room["pos"]}')
+            # print(f'    room rot: {room["rot"]}')
+            # print(f'    room scale: {room["scale"]}')
+            # print(f'    room empty: {room["empty"]}')
+            # print(f'    room obj num: {len(room["children"])}')
             room_info = {'room_type': room['type'],  # 'Bedroom', 'MasterBedroom', 'SecondBedroom', ...
-                         'room_id': room['instanceid'],  # not used
+                         'room_id': room['instanceid'],  # 'Library-4425', not used
                          'object_list': []}  # list of object_info
-
-            # if room['type'] in all_room_dict:  # 测试用, 如果房间类型已经存在，则跳过, 保证每个房间类型只有一个
-            #     continue
 
             children = room['children']
             for child in children:
@@ -96,7 +115,6 @@ def main():
                         if obj_catagory in right_catagory_name:
                             obj_catagory = right_catagory_name
                 if obj_catagory not in catagory_name_2_super: # 如果补全后还是没有, 则跳过这个错误的物体名字
-                    # print(f'obj_catagory: {obj_catagory} not in catagory_name_2_super')
                     continue
                 super_category = catagory_name_2_super[obj_catagory]
                 object_info = {'obj_id': model_jid[idx],  # 3D-FUTURE-model id, can be used to access 3D-FUTURE .obj file
@@ -106,7 +124,19 @@ def main():
                                'orientation': np.array(child['rot']),  # [x, y, z, w]
                                'scale': np.array(child['scale'])}  # [x, y, z]
                 room_info['object_list'].append(object_info)
-
+                
+                if SAVE_HOUSE:
+                    transform = rr.Transform3D(
+                        translation=object_info['translation'],
+                        rotation=rr.Quaternion(xyzw=object_info['orientation']),
+                        scale=object_info['scale']
+                    )
+                    object_name = object_info['obj_catagory'].replace(' ', '')
+                    rr.log(f'{room["instanceid"]}/{object_name}', transform)
+                    rr.log(f'{room["instanceid"]}/{object_name}', rr.Asset3D(
+                        path=os.path.join(config.DATASET_3DFUTURE_MODEL_PATH, object_info['obj_id'], 'raw_model.obj')
+                    ))
+                
             # 保存一个房间的信息, 物体太少就跳过
             if len(room_info['object_list']) < MIN_OBJ_NUM:
                 continue
@@ -115,7 +145,11 @@ def main():
             else:
                 all_room_dict[room_info['room_type']] = [room_info]
 
-    # 保存所有房间的信息, 先整理成 npy 数组, 再保存
+        if SAVE_HOUSE:
+            print(f'house {layout} saved as 3DFRONT-layout-{layout}.rrd')
+            exit(0) # 保存了一个房子, 退出
+
+    # 保存所有房子的所有房间的信息, 先整理成 npy 数组, 再保存
     print(f'all room types: {all_room_dict.keys()}')
     if not os.path.exists(save_path):
         os.mkdir(save_path)
@@ -177,7 +211,7 @@ def main():
                 scale=object_info_tobe_saved['scale'][0][object_idx]
             )
             object_name = object_info_tobe_saved['obj_catagory'][0][object_idx]
-            object_name=re.sub(r"\s+", "", object_name)
+            object_name=object_name.replace(' ', '')
             rr.log(f'{object_name}', transform)
             rr.log(f'{object_name}', rr.Asset3D(
                 path=os.path.join(config.DATASET_3DFUTURE_MODEL_PATH, object_info_tobe_saved['obj_id'][0][object_idx], 'raw_model.obj')
